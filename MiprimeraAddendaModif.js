84b7bf969b6e3ad01d16b460fff920212e71a883
Pablo Rosas <pablorosas3115@gmail.com>
2022-11-10 10:12:28 -0600

    Sacando los Conceptos de la factura

--- MiprimeraAddendaModif.js
+++ MiprimeraAddendaModif.js
@@ -9,41 +9,60 @@ fs.readdir("./", (error, files) => {
     throw error;
   }
   var archivo = fs.readFileSync("./FacturaRecord.xml", "UTF-8");
+  var nodoBuscado = "<Concepto ";
+  var inicioCad = archivo.indexOf(nodoBuscado);
+  var segNodoBusc = "</Concepto>";
+  var finCad = archivo.indexOf(segNodoBusc);
+  var TamsegNodoBusc = segNodoBusc.length;
+  var finNOdo = finCad + TamsegNodoBusc;
+  var copPrimConc = archivo.slice(inicioCad, finNOdo); // console.log(copPrimConc);
+  var primParteCad = archivo.slice(0, inicioCad);
+  // var ultimaParteCad = archivo.slice(finNOdo );
+  // var newCad = primParteCad+ultimaParteCad; //console.log(newCad);
+
+  var contador = 0;
+  var contador2 = 0;
+  var posicion = archivo.indexOf("<Concepto "); //Pocision inicial de la cadena
+  var posicionFin = archivo.indexOf("</Concepto>"); //Pocision inicial de la cadena
+  console.log("Posicion inicial de etiqueta de apertura -->  " + posicion);
+  console.log("Posicion inicial de etiqueta de cierre --> "+ posicionFin);
+
+  var myArray = [];
+
+  while (posicion != -1  && posicionFin != -1) { //Si existe mis cadenas
+    var myArray2 = [];
+    var nodoCierre = "</Concepto>";
+    var tamNodoCierre = nodoCierre.length;
+    var concepto = archivo.slice(posicion, posicionFin+tamNodoCierre);
+      myArray2.push(concepto);
+      myArray.push(myArray2);
+    var posicion = archivo.indexOf("<Concepto ", posicion + 1); //Busca la coincidencia a partir de un Num dado
  
+    var  posicionFin = archivo.indexOf(nodoCierre, posicionFin + 1); //Busca la coincidencia a partir de un Num dado
 
-  for (i = 0; i < archivo.length; i++)
-   var contador = 0;
-  var consepto = "<";
-    if (i == consepto){
-       console.log(contador);
-       console.log(  contador +1)
-    }
- //  console.log(i + " : " + archivo.charAt(i));
-  
+   
 
+    console.log("Posision de nodo <Concepto apertura -->  " + posicion);
+    console.log("Posicion de ndo </Concepto> cierre --> " + posicionFin);
+  }
+console.log(myArray);
 });
 
 
+// var conceptos = [];
+//    while ( inicioCad != -1 )//Existe el nodo "<Concepto "
+//   {
+//        var pCad = archivo.slice(0, inicioCad); console.log(pCad);
+//        var segCad = archivo.slice(finNOdo); console.log(segCad);
+//        var concepto = archivo.slice(inicioCad, );
+//        conceptos.push()
+//        var seg = archivo.slice(0, finCad); console.log("segunda cadena" + seg);
+//       archivo = seg + segCad; //console.log(archivo);
 
+//      inicioCad = archivo.indexOf("<Concepto "); //console.log(nodoBuscado)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(archivo);
+// cadena = cadena.slice(0, posicion) + "atito" + cadena.slice(posicion + 3);
 
 // var miNodoAMeter = "<fx:Addenda>MXN</fx:Addenda>";
 // var NodoBuscado = '</fx:ComprobanteEx>';
