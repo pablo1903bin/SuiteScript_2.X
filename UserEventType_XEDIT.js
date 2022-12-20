12b47ba428a981200bd9615610d13143ca02a555
Pablo Rosas <pablorosas3115@gmail.com>
2022-12-20 16:14:14 -0600

    buscando los articulos en xml

--- UserEventType_XEDIT.js
+++ UserEventType_XEDIT.js
@@ -117,9 +117,73 @@ define(["N/record", "N/log", "N/file", "N/xml", "N/runtime"], function (
                     <FolioNotaEntrada>${FolioNotaEntrada}</FolioNotaEntrada>
                   </Remision>`;
           return NodoRemision;
-        }
+        } //End function Remision()
+        function Pedido() {
+          let ID = "Pedido";
+          let RowOrder = "data-R-Order";
+          let Proveedor = "data-Prov";
+          let Remision = "data-Rem";
+          let FolioPedido = "data-Fol-Ped";
+          let Tienda = "data-tiend";
+          let CantidadArticulos = "data-Cant-Art";
+          let PedidoEmitidoProveedor = "data-PedEm-Prov";
+
+          let Nodopedidos = `<Pedidos Id="${ID}" RowOrder="${RowOrder}">
+                         <Proveedor> ${Proveedor} </Proveedor>
+                         <Remision> ${Remision} </Remision>
+                         <FolioPedido> ${FolioPedido} </FolioPedido>
+                         <Tienda> ${Tienda} </Tienda>
+                         <CantidadArticulos> ${CantidadArticulos} </CantidadArticulos>
+                         <PedidoEmitidoProveedor> ${PedidoEmitidoProveedor} </PedidoEmitidoProveedor>
+                      </Pedidos>`;
+          return Nodopedidos;
+        } //End function Pedido()
+        function Articulos(concepto) {
+          //Recorrer la cadena consepto para sacar valores jeje
+          let Id = "data-Art";
+          let RowOrder = "data-R-Ord";
+          let Proveedor = "data-Prov";
+          let Remision = "data-Rem";
+          let FolioPedido = "data-Folio";
+          let Tienda = "data-Tien";
+          let Codigo = "data-Cod";
+          let CantidadUnidadCompra = "data-Cant";
+          let CostoNetoUnidadCompra = "data-CostNetUnComp";
+          let PorcentajeIEPS = "data-PorIeps";
+          let PorcentajeIVA = "data-Porc-IVA";
+
+          let NodoArticulos = `<Articulos Id="${Id}" RowOrder="${RowOrder}">
+                        <Proveedor> ${Proveedor} </Proveedor>
+                        <Remision> ${Remision} </Remision>
+                        <FolioPedido> ${FolioPedido} </FolioPedido>
+                        <Tienda> ${Tienda} </Tienda>
+                        <Codigo> ${Codigo} </Codigo>
+                        <CantidadUnidadCompra> ${CantidadUnidadCompra} </CantidadUnidadCompra>
+                        <CostoNetoUnidadCompra> ${CostoNetoUnidadCompra} </CostoNetoUnidadCompra>
+                        <PorcentajeIEPS> ${PorcentajeIEPS} </PorcentajeIEPS>
+                        <PorcentajeIVA> ${PorcentajeIVA} </PorcentajeIVA>
+                      </Articulos>`;
+          return NodoArticulos;
+        } //End function Articulos()
+        function RecConsepto() {
+          var posicion = xmlData.indexOf("<cfdi:Concepto "); //Pocision inicial de la cadena
+          var posicionFin = xmlData.indexOf("</cfdi:Concepto>"); //Pocision inicial de la cadena
+          var conseptosTotales = "";
+          while (posicion != -1 && posicionFin != -1) {
+            //Si existen mis cadenas
+            var nodoCierre = "</cfdi:Concepto>";
+            var tamNodoCierre = nodoCierre.length;
+            var concepto = xmlData.slice(posicion, posicionFin + tamNodoCierre);
+            var art = Articulos(concepto); //Llamo a mi funcion Articulos y almaceno su valor
+            conseptosTotales += art;
+            //Armar cada nodo individual y meterlo al mismo tiempo a la cadena de nodos
+            var posicion = xmlData.indexOf("<cfdi:Concepto ", posicion + 1); //Busca la coincidencia a partir de un Num dado
+            var posicionFin = xmlData.indexOf(nodoCierre, posicionFin + 1); //Busca la coincidencia a partir de un Num dado
+          }
+          return conseptosTotales;
+        } //End Function RecConsepto()
         let nodoPadre_DScargaReProv = `<DSCargaRemisionProv xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://tempuri.org/DSCargaRemisionProv.xsd" xmlns:xs="http://www.w3.org/2001/XMLSchema">
-                ${Remision()}</DSCargaRemisionProv>`;
+                ${Remision()}${Pedido()}${RecConsepto()}</DSCargaRemisionProv>`;
 
         let sttr = "<cfdi:Addenda>"; //MI nodo a buscar donde creare nuevos nodos hijos
         let tamSttr = sttr.length; //tamaño de mi nodo a buscar
@@ -180,7 +244,10 @@ define(["N/record", "N/log", "N/file", "N/xml", "N/runtime"], function (
 
       IdDocCert
         ? EvaluarCliente(cliente)
-        : log.debug("Aun no existe el doc Certificado");
+        : log.debug(
+            "Aun no existe el doc Certificado de este cliente",
+            cliente
+          );
     } //End funcion EditOnline()
 
     if (context.type == context.UserEventType.CREATE) {
